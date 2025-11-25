a2daadee418211e5bd5f9906afeb7878bd9788b8
Matt Bailey <mjbaileysolutions@gmail.com>
2025-11-26 09:11:04 +1100

    Adjust NavBar to be more center

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -5,19 +5,18 @@ import OrSeparator from "../ui/OrSeparator";
 
 export default function Hero(){
     return(
-        <section className="md:mx-5 w-full mt-10 md:mt-20">
-            <h1 className="md:text-8xl text-center">DO YOU KNOW THE POFFERMAN!</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2">
+        <section className="md:mx-5 w-full mt-10 md:mt-15">
+            <h1 className="md:text-7xl text-center text-primary dark:text-secondary-foreground">DO YOU KNOW THE POFFERMAN!</h1>
+            <div className="mx-auto min-h-40">
                 <Image 
-                src={"/insta-screenshot-1.png"} 
-                alt="Owner cooking some Pofferjies" 
-                width={500}
-                height={500}
-                className="rounded-lg w-full"
+                src={"/Logo.jpg"} 
+                alt="The PofferMan!" 
+                width={400}
+                height={400}
+                className="rounded-full max-h-4/5 min-h-1/2 mx-auto"
                 />
+            </div>
                 <div className="flex flex-col justify-center mx-5 py-5 text-center">
-                    <h2 className="my-5">The Pofferman?</h2>
-                    <h1 className="my-5">The Pofferman!</h1>
                     <p className="text-3xl my-5">Golden brown, Fluffy and Sweet, it`s a real joy to eat!</p>
 
                     <div className="flex flex-col md:flex-row justify-center min-h-25 items-center md:px-5 mt-10">
@@ -26,7 +25,6 @@ export default function Hero(){
                         <Button variant="default" size={"lg"} className="text-lg">Enquire About Booking!</Button>
                     </div>
                 </div>
-            </div>
         </section>
     )
 }
