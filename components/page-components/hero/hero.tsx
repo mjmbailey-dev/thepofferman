44c90f8b7d61e72b2efb06ad2cf8ea278f0b77ba
Matt Bailey <mjbaileysolutions@gmail.com>
2025-11-27 16:04:07 +1100

    Loyalty Card Section and Hero Image Adjustments

--- components/page-components/hero/hero.tsx
+++ components/page-components/hero/hero.tsx
@@ -8,21 +8,25 @@ import BrandButton from "../../ui/custom/BrandButton";
 
 export default function Hero(){
     return(
-        <section className="w-full bg-secondary-lighter h-auto flex justify-center py-20 ">
+        <section className="w-full bg-secondary-lighter h-auto flex justify-center py-10 md:py-20 ">
             <div className="max-w-7xl mt-20 mx-10 w-full">
-                <div className="border-4 rounded-3xl border-primary-dark flex flex-col lg:flex-row align-middle justify-between p-15">
+                <div className="border-4 rounded-3xl border-primary-dark flex flex-col lg:flex-row align-middle justify-between mx-5 md:mx-0 p-15">
                     <div className="flex flex-col align-middle items-center justify-between gap-4">
-                        <h1 className="header-style text-center">Do You Know The  <br/>PofferMan?!</h1>
+                        <h1 className="header-style text-center text-5xl! sm:text-6xl!">Do You Know The  <br/>PofferMan?!</h1>
                         <div className="flex flex-col gap-5">
                             <p className="text-style text-center">{"Golden brown, Fluffy and Sweet, it`s a real joy to eat!"}</p>
                             <p className="text-style text-center">The PofferMan brings traditional Dutch pancakes to life!</p>
                         </div>
-                        <div className="flex flex-row gap-5">
-                            <BrandButton variant={"secondary"} size={"lg"} className="min-w-[150px] sm:min-w-[200px]">Find Us</BrandButton>
-                            <BrandButton variant={"primary"} size={"lg"} className="min-w-[150px] sm:min-w-[200px]">Contact Us</BrandButton>
+                        <div className="sm:flex flex-row gap-5 hidden">
+                            <BrandButton variant={"secondary"} size={"lg"} className="min-w-[100px] xs:min-w-[150px] sm:min-w-[200px]">Find Us</BrandButton>
+                            <BrandButton variant={"primary"} size={"lg"} className="min-w-[100px] xs:min-w-[150px] sm:min-w-[200px]">Contact Us</BrandButton>
+                        </div>
+                        <div className="flex flex-row gap-5 sm:hidden">
+                            <BrandButton variant={"secondary"} size={"sm"} className="min-w-[100px] xs:min-w-[150px] sm:min-w-[200px]">Find Us</BrandButton>
+                            <BrandButton variant={"primary"} size={"sm"} className="min-w-[100px] xs:min-w-[150px] sm:min-w-[200px]">Contact Us</BrandButton>
                         </div>
                     </div>
-                    <div className="p-3 mx-auto my-10 min-h-50 min-w-[300px] flex align-middle items-center ">
+                    <div className="mx-auto my-10 min-h-50 min-w-[200px] md:min-w-[300px] flex align-middle items-center ">
                         <Image src="/Logo.jpg" alt="Company Logo" height={400} width={400} className="rounded-full logo-shadows"/>
                     </div>
                 </div>
